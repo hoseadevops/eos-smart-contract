eefb13708b7d28dd29d1b82e50dba18819f1d95e
hosea <starit@msn.cn>
2018-11-23 11:59:21 +0800

    init

--- docker/eosio/container.sh
+++ docker/eosio/container.sh
@@ -83,7 +83,7 @@ function cli()
 {
     local cmd=$2
     local ip=$(get_keosd_ip)
-    run_cmd "docker exec -it nodeos-$eosio_container bash -c \"/opt/eosio/bin/cleos -u http://0.0.0.0:8888 --wallet-url http://$ip:9876 $cmd\""
+    run_cmd "docker exec -it nodeos-$eosio_container /opt/eosio/bin/cleos -u http://0.0.0.0:8888 --wallet-url http://$ip:9876 $cmd"
 }
 
 function _init_contract()
@@ -94,6 +94,23 @@ function _init_contract()
     run_cmd "sh eos.sh cli 'set contract eosio.msig $project_docker_persistent_dir/contracts/eosio.msig -x 1000s -p eosio.msig@active'"
     _open_un_lock_wallet
     run_cmd "sh eos.sh cli 'set contract eosio $project_docker_persistent_dir/contracts/eosio.bios -x 1000s -p eosio@active'"
+    _open_un_lock_wallet
+    _build
+}
+
+function _build()
+{
+    # Bootstrap new chain
+    sh eos.sh cli "push action eosio.token create '[ \"eosio\", \"10000000000.0000 SYS\" ]' -p eosio.token"
+    sh eos.sh cli "push action eosio.token issue '[ \"eosio\", \"1000000000.0000 SYS\", \"memo\" ]' -p eosio"
+    sh eos.sh cli "push action eosio setpriv '[\"eosio.msig\", 1]' -p eosio@active"
+    sh eos.sh cli "set contract eosio ./docker/persistent/contracts/eosio.system/ -x 1000s -p eosio@active"
+
+    # Deploy eosio.wrap
+    # sh test.sh cli "wallet import -n hexing_wallet --private-key 5J3JRDhf4JNhzzjEZAsQEgtVuqvsPPdZv4Tm6SjMRx1ZqToaray"
+    sh eos.sh cli "system newaccount eosio eosio.wrap EOS7LpGN1Qz5AbCJmsHzhG7sWEGd9mwhTXWmrYXqxhTknY2fvHQ1A --stake-cpu \"50 SYS\" --stake-net \"10 SYS\" --buy-ram-kbytes 5000 --transfer"
+    sh eos.sh cli "push action eosio setpriv '[\"eosio.wrap\", 1]' -p eosio@active"
+    sh eos.sh cli "set contract eosio.wrap ./docker/persistent/contracts/eosio.sudo/"
 }
 
 function _network_create()
---
+++ test.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+function read_kv_config()
+{
+    local file=$1
+    local key=$2
+    cat $file | grep "$key=" | awk -F '=' '{print $2}'
+}
+
+NODEOS_PORT=$(read_kv_config .env NODEOS_PORT)
+VERSION=$(read_kv_config .env VERSION)
+
+project_path=$(cd $(dirname $0); pwd -P)
+project_docker_path="$project_path/docker"
+source $project_docker_path/bash.sh
+developer_name=$('whoami');
+
+
+app_basic_name=eos-dev
+app="$app_basic_name-$developer_name"
+
+eosio_image=eosio/eos:$VERSION
+
+# container
+eosio_container=$app
+
+# container dir
+project_docker_eosio_dir="$project_docker_path/eosio"
+
+project_docker_runtime_dir="$project_docker_path/runtime"
+project_docker_persistent_dir="$project_docker_path/persistent"
+
+project_test_dir="$project_path/test"
+
+#---------- eosio container ------------#
+source $project_docker_path/eosio/container.sh
+source $project_docker_path/eosio/wallet.sh
+source $project_docker_path/eosio/account.sh
+source $project_docker_path/eosio/contract.sh
+
+
+function assert()
+{
+    if [ $1 -eq 0 ]; then
+        FAIL_LINE=$( caller | awk '{print $1}')
+        echo "Assertion failed. Line $FAIL_LINE:"
+        head -n $FAIL_LINE $BASH_SOURCE | tail -n 1
+        exit 99
+    fi
+}
+
+function deploy()
+{
+
+}
+
+function help()
+{
+cat <<EOF
+    Usage: sh eos.sh [options]
+
+        Valid options are:
+
+        restart
+        clean
+
+
+        open_unlock_wallet
+EOF
+}
+
+action=${1:-help}
+ALL_COMMANDS="build restart clean cpp cli deploy key_create send_cmd_to_eos_container open_unlock_wallet key_create get_keosd_ip"
+list_contains ALL_COMMANDS "$action" || action=help
+$action "$@"
